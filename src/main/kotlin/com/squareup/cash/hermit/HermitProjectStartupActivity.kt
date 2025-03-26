93a1d2142c6b597a46b7723bc820328105196704
Sutina Wipawiwat <swipawiwat@squareup.com>
2025-03-26 18:39:40 +1100

    chore: Remove projectClosing with Disposable
    as per https://plugins.jetbrains.com/docs/intellij/plugin-components.html?_gl=1%2A19w0afu%2A_gcl_au%2AMTg1NDI5Nzc0Ny4xNzM3NjkzMzky%2AFPAU%2AMTg1NDI5Nzc0Ny4xNzM3NjkzMzky%2A_ga%2ANTIyNzM4NTg5LjE2OTcwMDQ1ODc.%2A_ga_9J976DJZ68%2AMTc0Mjk3MDQ2Ny44NC4wLjE3NDI5NzA0NjcuNjAuMC4w#project-and-application-close

--- src/main/kotlin/com/squareup/cash/hermit/HermitProjectStartupActivity.kt
+++ src/main/kotlin/com/squareup/cash/hermit/HermitProjectStartupActivity.kt
@@ -6,5 +6,8 @@ import com.intellij.openapi.startup.ProjectActivity
 class HermitProjectStartupActivity: ProjectActivity {
   override suspend fun execute(project: Project) {
     Hermit(project).open()
+
+    // Ensure HermitCleanupService is initialized and disposal is registered
+    project.getService(HermitCleanupService::class.java)
   }
 }
